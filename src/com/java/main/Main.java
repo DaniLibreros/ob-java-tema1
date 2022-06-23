197d79fb7d19ed94780a81ed93ecb5a130195ee0
Dlibreros <libreros1226@gmail.com>
2022-06-23 22:03:40 +0200

    este es el primer commit con funcionalidad del tema uno.

---
+++ src/com/java/main/Main.java
@@ -0,0 +1,50 @@
+package com.java.main;
+
+public class Main {
+    public static <string> void main(String[] args) {
+        // enters
+        byte numberB = 11;
+        System.out.println( numberB );
+
+        short numberS = 1;
+        System.out.println( numberS );
+
+        int numberI = 1;
+        System.out.println( numberI );
+
+        long numberL = 1;
+        System.out.println( numberL );
+
+        // punto flotante
+        float decimal = 1.5f;
+        System.out.println( decimal );
+
+        double decimalD = 11.11;
+        System.out.println( decimalD );
+
+
+        //caracter
+        String stringVariable = "string";
+        System.out.println( stringVariable );
+
+        char isChar = 'a';
+        System.out.println( isChar );
+
+        //boleanos
+        boolean verdadero = true;
+        System.out.println( verdadero );
+
+        boolean falso = false;
+        System.out.println( falso );
+
+        //cadenas de texto
+
+        String name = "Dani";
+        String lastName = "libreros";
+        System.out.println( name + lastName );
+
+        //tipos de envoltorio
+        long numberlong = 2L;
+        System.out.println( numberlong );
+    }
+}
